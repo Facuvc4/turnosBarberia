febe037e17b66318a41856f2ca7928a032f7ccae
Facu <facundolopez026@gmail.com>
2024-09-21 16:17:21 -0300

    Insertando home

--- src/controllers/home/index.controller.ts
+++ src/controllers/home/index.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from 'express';
+import getDates from '../../helpers/date/get.all.helper';
 
 const main = async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.cookies.token;
-    if (!token) res.redirect('/login');
-    res.render('home');
-}
+  const token = req.cookies.token;
+  if (!token) res.redirect('/login');
+  const dates = await getDates();
+  res.render('home', { dates });
+};
 
 export default main;
