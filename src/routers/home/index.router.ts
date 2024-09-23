36069eb33751672317a189e3e301c3ccdb2d95c2
Facu <facundolopez026@gmail.com>
2024-09-23 09:29:34 -0300

    Arreglando redirecciones

--- src/routers/home/index.router.ts
+++ src/routers/home/index.router.ts
@@ -5,7 +5,7 @@ import ownShiftsController from '../../controllers/home/own.shifts.controller';
 const homeRouter = express.Router();
 
 homeRouter.get('/', (req, res) => {
-  if (!req.cookies.token) res.redirect('/login');
+  if (!req.cookies.token) return res.redirect('/login');
   res.render('home', { hideNavbar: false });
 });
 
@@ -14,12 +14,12 @@ homeRouter.get('/shifts', allShiftsController);
 homeRouter.get('/shifts/own', ownShiftsController);
 
 homeRouter.get('/login', (req, res) => {
-  if (req.cookies.token) res.redirect('/');
+  if (req.cookies.token) return res.redirect('/');
   res.render('login', { hideNavbar: true });
 });
 
 homeRouter.get('/register', (req, res) => {
-  if (req.cookies.token) res.redirect('/');
+  if (req.cookies.token) return res.redirect('/');
   res.render('register', { hideNavbar: true });
 });
 
